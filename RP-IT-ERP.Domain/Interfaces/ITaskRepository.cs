f6ff03da045d19158d633a0a11e29540745467e7
Robert Poręba <webberr14@gmail.com>
2022-01-11 18:38:36 +0100

    Add repository Interfaces

---
+++ RP-IT-ERP.Domain/Interfaces/ITaskRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RP_IT_ERP.Domain.Interfaces
+{
+    public interface ITaskRepository
+    {
+        int AddTask(RP_IT_ERP.Domain.Model.Task task);
+    }
+}
---
+++ RP-IT-ERP.Domain/Interfaces/IWarehouseRepository.cs
@@ -0,0 +1,14 @@
+﻿using RP_IT_ERP.Domain.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RP_IT_ERP.Domain.Interfaces
+{
+    public interface IWarehouseRepository
+    {
+        int AddWarehouseItem(Warehouse warehouseItem);
+    }
+}
