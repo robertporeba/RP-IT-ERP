b06234ad8f4c3386e92700e277539b71b6aadb28
Robert Poręba <webberr14@gmail.com>
2022-01-12 19:53:11 +0100

    Add Infrastructure Dependency Injection

--- RP-IT-ERP.Infrastructure/DependencyInjection.cs
+++ RP-IT-ERP.Infrastructure/DependencyInjection.cs
@@ -14,6 +14,13 @@ namespace RP_IT_ERP.Infrastructure
         public static IServiceCollection AddInfrastructure(this IServiceCollection services)
         {
             services.AddTransient<ITaskRepository, TaskRepository>();
+            services.AddTransient<IClientRepository, ClientRepository>();
+            services.AddTransient<IElementRepository, ElementRepository>();
+            services.AddTransient<IRaportTypeRepository, RaportTypeRepository>();
+            services.AddTransient<IRaportRepository, RaportRepository>();
+            services.AddTransient<ITaskStatusRepository, TaskStatusRepository>();
+            services.AddTransient<ITaskTypeRepository, TaskTypeRepository>();
+            services.AddTransient<IWarehouseRepository, WarehouseRepository>();
             return services;
         }
     }
