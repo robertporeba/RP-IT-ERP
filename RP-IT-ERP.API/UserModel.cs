80800e3d9740b1be9e6c774d44c3fa5a146b471c
Robert Poręba <webberr14@gmail.com>
2022-01-12 19:53:49 +0100

    Add user model

---
+++ RP-IT-ERP.API/UserModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RP_IT_ERP.API
+{
+    public class UserModel
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Type { get; set; }
+    }
+}
