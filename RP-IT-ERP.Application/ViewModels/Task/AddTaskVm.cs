93ed406d6c9cef732abe58b355dcf11de5182fb8
Robert Poręba <webberr14@gmail.com>
2022-01-19 19:10:18 +0100

    Add task view models

---
+++ RP-IT-ERP.Application/ViewModels/Task/AddTaskVm.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using RP_IT_ERP.Application.Mapping;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RP_IT_ERP.Application.ViewModels.Task
+{
+    public class AddTaskVm : IMapFrom<RP_IT_ERP.Domain.Model.Task>
+    {
+        public DateTime StartDate { get; set; }
+        public DateTime FinishDate { get; set; }
+        public string Name { get; set; }
+        public int ClientId { get; set; }
+        public int TaskStatusId { get; set; }
+        public int TaskTypeId { get; set; }
+
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<RP_IT_ERP.Domain.Model.Task, AddTaskVm>().ReverseMap();
+        }
+    }
+}
---
+++ RP-IT-ERP.Application/ViewModels/Task/UpdateTaskVm.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using RP_IT_ERP.Application.Mapping;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RP_IT_ERP.Application.ViewModels.Task
+{
+    public class UpdateTaskVm : IMapFrom<RP_IT_ERP.Domain.Model.Task>
+    {
+        public int Id { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime FinishDate { get; set; }
+        public string Name { get; set; }
+        public int ClientId { get; set; }
+        public int TaskStatusId { get; set; }
+        public int TaskTypeId { get; set; }
+
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<RP_IT_ERP.Domain.Model.Task, UpdateTaskVm>().ReverseMap();
+        }
+    }
+}
